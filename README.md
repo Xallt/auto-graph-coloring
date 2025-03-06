e67d05aab6f95bf4efc5c53a9a2e0bb81112b73f
Dmitry Shabat <mitya.shabat@gmail.com>
2025-03-06 20:10:38 +0200

    Initial implementation of Auto Graph Coloring plugin

--- README.md
+++ README.md
@@ -1,94 +1,56 @@
-# Obsidian Sample Plugin
+# Auto Graph Coloring for Obsidian
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This plugin automatically assigns random, visually distinct colors to groups in the Obsidian graph view.
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## Features
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- Adds a command "Auto Color Groups" to the command palette
+- Automatically assigns visually distinct colors to all groups in the graph view
+- Uses a color generation algorithm that maximizes the visual difference between colors
+- Works with both global and local graph views
 
-## First time developing plugins?
+## How to Use
 
-Quick starting guide for new plugin devs:
+1. First, create groups in your graph view:
+   - Open the graph view in Obsidian
+   - Click on the "Groups" section in the graph settings
+   - Add groups based on folders, tags, or custom queries
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+2. Run the "Auto Color Groups" command:
+   - Press `Ctrl+P` (or `Cmd+P` on Mac) to open the command palette
+   - Search for "Auto Color Groups" and select it
+   - The plugin will automatically assign distinct colors to all your groups
 
-## Releasing new releases
+3. The graph view will refresh automatically with the new colors applied
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+## How It Works
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+The plugin uses the golden ratio conjugate (approximately 0.618033988749895) to generate colors with maximum visual distinction. This approach ensures that each color is as different as possible from the others, making it easier to distinguish between different groups in your graph.
 
-## Adding your plugin to the community plugin list
+## Installation
 
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+### From Obsidian Community Plugins
 
-## How to use
+1. Open Obsidian Settings
+2. Go to Community Plugins
+3. Click "Browse" and search for "Auto Graph Coloring"
+4. Click "Install"
+5. Enable the plugin
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+### Manual Installation
 
-## Manually installing the plugin
+1. Download the latest release from the GitHub repository
+2. Extract the files to your Obsidian vault's `.obsidian/plugins/auto-graph-coloring` directory
+3. Enable the plugin in Obsidian's Community Plugins settings
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+## Development
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
+This plugin is built using the Obsidian Plugin API. To build it from source:
 
-## Funding URL
+1. Clone the repository
+2. Run `npm install` to install dependencies
+3. Run `npm run build` to build the plugin
 
-You can include funding URLs where people who use your plugin can financially support it.
+## License
 
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+This project is licensed under the MIT License - see the LICENSE file for details.
--- main.ts
+++ main.ts
@@ -1,85 +1,33 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, Notice } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface AutoGraphColoringSettings {
+	// We might add settings in the future
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AutoGraphColoringSettings = {
+	// Default settings would go here
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutoGraphColoringPlugin extends Plugin {
+	settings: AutoGraphColoringSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add a command to auto-color groups
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'auto-color-groups',
+			name: 'Auto Color Groups',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.autoColorGroups();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		// Nothing to clean up
 	}
 
 	async loadSettings() {
@@ -89,46 +37,95 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	/**
+	 * Generate a visually distinct color in RGB format
+	 * @param index The index of the color in the sequence
+	 * @param total Total number of colors to generate
+	 * @returns RGB color value as a number
+	 */
+	generateDistinctColor(index: number, total: number): number {
+		// Use HSV color space for better visual distinction
+		// Convert to RGB for Obsidian's API
+		const goldenRatioConjugate = 0.618033988749895;
+		const hue = (index * goldenRatioConjugate) % 1.0;
+
+		// Convert HSV to RGB
+		// Using saturation and value of 0.95 for vibrant but not too bright colors
+		const saturation = 0.95;
+		const value = 0.95;
+
+		let r: number, g: number, b: number;
+
+		const h = hue * 6;
+		const i = Math.floor(h);
+		const f = h - i;
+		const p = value * (1 - saturation);
+		const q = value * (1 - f * saturation);
+		const t = value * (1 - (1 - f) * saturation);
+
+		switch (i % 6) {
+			case 0: r = value; g = t; b = p; break;
+			case 1: r = q; g = value; b = p; break;
+			case 2: r = p; g = value; b = t; break;
+			case 3: r = p; g = q; b = value; break;
+			case 4: r = t; g = p; b = value; break;
+			case 5: r = value; g = p; b = q; break;
+			default: r = 0; g = 0; b = 0;
+		}
+
+		// Convert to RGB integer
+		const rgb = Math.round(r * 255) << 16 | Math.round(g * 255) << 8 | Math.round(b * 255);
+		return rgb;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	/**
+	 * Auto-color the groups in the graph view
+	 */
+	async autoColorGroups() {
+		try {
+			// Access the graph plugin using the app object
+			// We need to use 'any' type here because internalPlugins is not exposed in the public API
+			const app = this.app as any;
+			const graphPlugin = app.internalPlugins.plugins.graph;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+			if (!graphPlugin) {
+				new Notice('Graph plugin not found');
+				return;
+			}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+			// Load current graph settings
+			const settings = await graphPlugin.loadData();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+			// Check if there are any color groups
+			if (!settings.colorGroups || settings.colorGroups.length === 0) {
+				new Notice('No color groups found in the graph. Please create some groups first.');
+				return;
+			}
 
-	display(): void {
-		const {containerEl} = this;
+			// Assign random colors to each group
+			const totalGroups = settings.colorGroups.length;
+			settings.colorGroups.forEach((group: any, index: number) => {
+				group.color = {
+					a: 1,
+					rgb: this.generateDistinctColor(index, totalGroups)
+				};
+			});
 
-		containerEl.empty();
+			// Save the updated settings
+			await graphPlugin.saveData(settings);
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			// Refresh the graph view
+			// First detach any existing graph views
+			this.app.workspace.detachLeavesOfType('graph');
+			// Then disable and re-enable the graph plugin to apply changes
+			await graphPlugin.disable();
+			await graphPlugin.enable();
+
+			new Notice(`Successfully assigned colors to ${totalGroups} groups`);
+		} catch (error) {
+			console.error('Error auto-coloring groups:', error);
+			new Notice('Error auto-coloring groups. Check console for details.');
+		}
 	}
 }
